900fa8792e6b20373889ad5a767080a7fd6bae1f
yuuz24 <yujin342@naver.com>
2025-08-07 03:19:03 +0900

    feat: Security의 Confidentiality 중 https 통신 적용 확인 내용 구현
    - 루틴 JSON 파일 Backend/Playwright 각각 작성
    - runner.py에 security 추가 *TraceBack 출력으로 인해 try~except 추가
    - parser.py JSON 읽기 오류로 인해 return 형식 수정

--- src/core/runner.py
+++ src/core/runner.py
@@ -2,14 +2,24 @@
 # 1 from assessments import performance # 이 줄을 아래와 같이 수정!
 from assessments import performance
 from assessments import usability # usability.py가 있다면 함께 임포트
+from assessments import security
 
 
 def run_routine(routine, driver):
     for step in routine:
         assessment = step["assessment"]
-        if assessment == "performance":
-            performance.check(driver, step)
-        elif assessment == "usability": # usability 추가
-            usability.check(driver, step)
-        else:
-            print(f"[SKIP] 지원하지 않는 assessment: {assessment}")
+        # yujin 25.08.07 Traceback 문구 출력 피하기 위해 try~except 블록으로 감쌈
+        try:
+            if assessment == "performance":
+                performance.check(driver, step)
+            elif assessment == "usability": # usability 추가
+                usability.check(driver, step)
+            # 25.08.07 Security 추가
+            elif assessment == "security":
+                security.check(driver, step)
+            else:
+                print(f"[SKIP] 지원하지 않는 assessment: {assessment}")
+        except AssertionError as e:
+            print(f"[FAIL] 검사 실패: {e}")
+        except Exception as e:
+            print(f"[ERROR] 예기치 않은 오류 발생: {e}")
